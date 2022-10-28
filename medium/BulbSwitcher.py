e1ed0e84bd0761ba500a35d19f673c4cdd54f68c
gholtzap <gholtzap@asu.edu>
2022-10-27 19:38:19 -0700

    BulbSwitcher

---
+++ medium/BulbSwitcher.py
@@ -0,0 +1,10 @@
+# 35 / 35 test cases passed.
+# Status: Accepted
+# Runtime: 56 ms - top 50%
+# Memory Usage: 13.8 MB - top 5%
+# Language: Python
+
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+         return int(math.sqrt(n))
+        
