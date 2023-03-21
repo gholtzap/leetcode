b0cfbc9e57abc5f4b2370ec6c3a4ba4aff9c136d
gholtzap <gholtzap@asu.edu>
2023-03-21 00:47:03 -0700

    update

--- easy/matrix_diagonal_sum.java
+++ easy/matrix_diagonal_sum.java
@@ -1,18 +1,17 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
 
-        int output = 0, y = 0, len = mat.length-1, mid = mat.length/2;
+        int output = 0, y = 0;
 
         // iterate across the matrix, adding each diagonal # to output
         for(int i = 0; i<mat.length; i++){
-            output+=mat[i][y] + mat[i][len];
-            len--;
+            output+=mat[i][y] + mat[i][mat.length-i-1];
             y++;
         }
 
         // if matrix length is even, subtract the middle value
         if(mat.length%2 == 1){
-            output -= mat[mid][mid];
+            output -= mat[mat.length/2][mat.length/2];
         }
 
         return output;
