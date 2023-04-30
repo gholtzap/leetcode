66bf49ee2cac30e0a0200384d5d0c921478c62d2
gholtzap <gholtzap@asu.edu>
2023-04-29 17:35:49 -0700

    most water improved

--- blind_75/two_pointers/container_with_most_water.py
+++ blind_75/two_pointers/container_with_most_water.py
@@ -1,4 +1,24 @@
-# Solution 1: O(n^2) O(1) 
+# Solution 1: O(N) O(1)
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+
+        highest = 0
+        left,right = 0,len(height)-1
+
+        while left < right:
+            area = (right-left) * min(height[left],height[right])
+            highest = max(highest, area)
+
+            if height[left] < height[right]:
+                left+=1
+            else:
+                right-=1
+
+        return highest
+
+
+# Solution 2: O(n^2) O(1) 
 # Way too slow. Not practical
 
 class Solution:
