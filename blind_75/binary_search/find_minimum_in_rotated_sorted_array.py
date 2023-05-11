ceebebcac6d6c36189527600409345487f45a455
gholtzap <gholtzap@asu.edu>
2023-05-11 16:11:44 -0700

    find min in sorted array sol2

--- blind_75/binary_search/find_minimum_in_rotated_sorted_array.py
+++ blind_75/binary_search/find_minimum_in_rotated_sorted_array.py
@@ -7,3 +7,29 @@ class Solution:
 
 
 # To do this prblem authentically, basically we need to replace nums.sort() with the most efficient sorting algorithm.
+# We can use quicksort like so:
+# Keep in mind that although this is more efficient on larger samples, the leetcode testcases are small and prefer binary sort
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+
+        return quicksort(nums)[0]
+
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr.pop()
+
+    items_greater = []
+    items_lower = []
+
+    for item in arr:
+        if item > pivot:
+            items_greater.append(item)
+
+        else:
+            items_lower.append(item)
+
+    return quicksort(items_lower) + [pivot] + quicksort(items_greater)
