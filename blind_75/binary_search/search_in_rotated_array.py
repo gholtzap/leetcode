224f0d44d656b1f194090a6ba7f55c48593c8ea7
gholtzap <gholtzap@asu.edu>
2023-05-11 16:26:49 -0700

    search in rotated array sol2

--- blind_75/binary_search/search_in_rotated_array.py
+++ blind_75/binary_search/search_in_rotated_array.py
@@ -5,3 +5,16 @@ class Solution:
         if target in nums:
             return nums.index(target)
         return -1
+    
+    
+# Slightly faster
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        for x in nums:
+            if x == target:
+                return nums.index(target)
+        return -1
+
+    
