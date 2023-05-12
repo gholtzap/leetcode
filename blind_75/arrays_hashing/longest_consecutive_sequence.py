ed3a95412b720d149beff24e96392a94a6656ede
gholtzap <gholtzap@asu.edu>
2023-05-12 09:43:54 -0700

    longest consecutive sequence sol2

--- blind_75/arrays_hashing/longest_consecutive_sequence.py
+++ blind_75/arrays_hashing/longest_consecutive_sequence.py
@@ -17,3 +17,24 @@ class Solution:
             count = 0
             
         return output
+    
+    
+# Solution 2 (just solution 1 except faster)
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+
+        numsSet = set(nums)
+        output, count = 0,0
+
+        for x in numsSet:
+            if x-1 not in numsSet:
+                pivot = x
+                while pivot in numsSet:
+                    count+=1
+                    pivot+=1
+           
+            output = max(output,count)
+            count = 0
+            
+        return output
