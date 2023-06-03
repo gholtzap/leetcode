91f3b7940fab600f892a304f49763a7ffb0e093f
gholtzap <gholtzap@asu.edu>
2023-06-03 00:36:46 -0400

    find diff of two arrays sol3

--- leetcode_75/hashmap-set/find_difference_of_two_arrays.py
+++ leetcode_75/hashmap-set/find_difference_of_two_arrays.py
@@ -44,3 +44,26 @@ class Solution:
 
         return [output1,output2] 
             
+            
+# Solution 3
+# O(n) | O(n)
+
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+
+
+        hashset1 = set(nums1)
+        hashset2 = set(nums2)
+
+        output1,output2 = set(),set()
+
+        for x in nums1:
+            if x not in hashset2 and x not in output1:
+                output1.add(x)
+
+        for x in nums2:
+            if x not in hashset1 and x not in output2:
+                output2.add(x)
+
+
+        return [list(output1),list(output2)]
