aed857cdfe502ef88a18a89d708b98ad6cf19234
gholtzap <gholtzap@asu.edu>
2023-06-24 10:13:33 -0400

    max width ramp sol2

--- medium/maximum_width_ramp.py
+++ medium/maximum_width_ramp.py
@@ -1,5 +1,22 @@
 # Solution 1
-# O(n^2) | O(n)
+# O(n log(n)) | O(n)
+
+class Solution:
+    def maxWidthRamp(self, nums:List[int]) -> int:
+        stack, r = [], 0
+        for i, a in enumerate(nums):
+            if not stack or nums[stack[-1]] > a:
+                stack.append(i)
+
+        print(stack)
+        
+        for j in range(len(nums) - 1, -1, -1):
+            while stack and nums[stack[-1]] <= nums[j]:
+                r = max(r, j - stack.pop())
+        return r
+
+# Solution 2
+# O(n^2) | O(1)
 
 class Solution:
     def maxWidthRamp(self, nums: List[int]) -> int:
