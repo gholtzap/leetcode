29519496ec6e0faf408a96441a99d53caf278246
gholtzap <gholtzap@asu.edu>
2023-07-03 23:53:20 -0400

    set matrix zeroes sol2

--- medium/set_matrix_zeroes.py
+++ medium/set_matrix_zeroes.py
@@ -1,6 +1,35 @@
 # Solution 1
 # O(n^2) | O(n)
 
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+        rows,columns = [],[]
+        l = len(matrix[0])
+        
+        for i in range(len(matrix)):
+            for j in range(l): 
+                if matrix[i][j] == 0:
+                    rows.append(i)
+                    columns.append(j)
+
+        for i in range(len(matrix)):
+            if i in rows:
+                matrix[i] = [0] * l
+                continue
+            for j in range(l):
+                if j in columns:
+                    for k in range(0,len(matrix)):
+                        matrix[k][j] = 0
+                    continue
+            
+
+# Solution 2
+# O(n^2) | O(n)
+
 
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
