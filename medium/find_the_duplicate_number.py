97a76d3d206a7922fa720c94a71a4c413d15002c
gholtzap <gholtzap@asu.edu>
2023-07-07 13:34:33 -0400

    find duplicate number sol2

--- medium/find_the_duplicate_number.py
+++ medium/find_the_duplicate_number.py
@@ -12,3 +12,15 @@ class Solution:
             hashmap[nums[i]] = 0
         return 0
 
+# Solution 2
+# O(n log(n)) | O(1)
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+ 
+        nums.sort()
+
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return nums[i]
+            
