e0dbcdd2e12366ec69386b89a75ce8aa72247c0b
gholtzap <gholtzap@asu.edu>
2023-07-24 13:32:55 -0400

    increasing triplet subseq

---
+++ leetcode_75/array-string/increasing_triplet_subsequence.py
@@ -0,0 +1,16 @@
+# Solution 1
+# O(n) | O(1)
+
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first = second = float('inf')
+        
+        for num in nums:
+            if num <= first:
+                first = num
+            elif num <= second:
+                second = num
+            else:
+                return True
+        return False
