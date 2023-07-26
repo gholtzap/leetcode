c832070b8dbc195a283ce0e0c16b3f28693cf257
gholtzap <gholtzap@asu.edu>
2023-07-26 00:23:06 -0400

    group anagrams sol2

--- blind_75/arrays_hashing/group_anagrams.py
+++ blind_75/arrays_hashing/group_anagrams.py
@@ -14,3 +14,29 @@ class Solution:
             res[tuple(count)].append(s)
 
         return res.values()
+    
+    
+# Solution 2
+# O(n^2 log(n)) | O(n)
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        output,hashmap = [],{}
+
+        for i,x in enumerate(strs):
+            x = list(x)
+            x.sort()
+            x = "".join(x)
+            if x not in hashmap:
+                hashmap[x] = [i]
+            else:
+                hashmap[x].append(i)
+
+        for x in hashmap:
+            temp = []
+            for y in hashmap[x]:
+                temp.append(strs[y])
+            output.append(temp)
+
+        return output
