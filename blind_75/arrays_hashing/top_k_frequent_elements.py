694f9a3ff632f1f616bcff428e1dc027fcccf092
gholtzap <gholtzap@asu.edu>
2023-07-27 11:42:47 -0400

    top k freq elements sol2

--- blind_75/arrays_hashing/top_k_frequent_elements.py
+++ blind_75/arrays_hashing/top_k_frequent_elements.py
@@ -1,26 +1,37 @@
 # Solution 1
 # O(n log n) | O(n)
 
+import heapq
+from collections import Counter
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = Counter(nums) 
+        return heapq.nlargest(k, count.keys(), key=count.get)
 
-        nums.sort()
-        temp,output = [],[]
+
+# Solution 2
+# O(n log n) | O(n)
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
         hashmap = {}
 
         for x in nums:
             if x not in hashmap:
-                hashmap[x]=1
+                hashmap[x] = 1
             else:
-                hashmap[x]+=1
+                hashmap[x] +=1
+
+        temp = []
 
         for x in hashmap:
             temp.append([hashmap[x],x])
 
         temp.sort()
 
+        output = []
         for i in range(len(temp)-1,len(temp)-1-k,-1):
             output.append(temp[i][1])
 
