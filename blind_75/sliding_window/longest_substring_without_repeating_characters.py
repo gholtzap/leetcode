9b6222840c87eb20cd2c6988afd09a23e41da651
gholtzap <gholtzap@asu.edu>
2023-07-28 11:41:59 -0400

    longest substring sol2

--- blind_75/sliding_window/longest_substring_without_repeating_characters.py
+++ blind_75/sliding_window/longest_substring_without_repeating_characters.py
@@ -1,3 +1,31 @@
+# Solution 1
+# O(n^3) | O(n)
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        
+        if len(s) == 0 or len(s) == 1:
+            return len(s)
+
+        hashmap,output = {},1
+
+        l,r = 0,0
+        while r < len(s):
+            r+=1
+            temp = s[l:r]
+            #print(f"Now testing {temp}")
+            if not len(temp) == len(set(temp)):
+                l+=1
+                continue
+            output = max(len(temp),output)  
+
+        return output
+
+
+# Solution 2
+# O(n) | O(n)
+
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
 
