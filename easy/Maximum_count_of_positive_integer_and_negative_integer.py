a5db5b99120f87c00329e2969aec0176d874734b
gholtzap <gholtzap@asu.edu>
2023-08-04 00:22:05 -0400

    .

--- easy/Maximum_count_of_positive_integer_and_negative_integer.py
+++ easy/Maximum_count_of_positive_integer_and_negative_integer.py
@@ -16,5 +16,13 @@ class Solution:
 
         return max(pos,neg)
     
+# Solution 2
+# O(n) | O(n)
+
+
+class Solution:
+    def maximumCount(self, nums: List[int]) -> int:
+       return max(len([x for x in nums if x > 0]), len([x for x in nums if x < 0]))
+
 
 
