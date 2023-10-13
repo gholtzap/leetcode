aadae61cafaca199941ebdda949984c3e0066c47
gholtzap <gholtzap@asu.edu>
2023-10-13 10:26:53 -0700

    .

--- easy/substrings_of_three_size_with_distinct_chars.py
+++ easy/substrings_of_three_size_with_distinct_chars.py
@@ -1,5 +1,27 @@
 # Solution 1
-# O(n^3) | O(n)
+# O(n) | O(1)
+
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+
+        output = 0
+
+        if len(s) < 3:
+            return 0
+
+        for i in range(len(s)-2):
+            temp = set()
+            for j in range(i,i+3): 
+                temp.add(s[j])
+            if len(temp) == 3:
+                output+=1
+
+        return output
+        
+
+
+# Solution 2
+# O(n^2) | O(1)
 
 class Solution:
     def countGoodSubstrings(self, s: str) -> int:
